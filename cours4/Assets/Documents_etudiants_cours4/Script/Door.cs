d9cb77722c3f3ec2b23d226db85ba86f59ef418c
B533P-18\533 <asd@hotmail.com>
2018-08-31 15:56:44 -0400

    remise projet

---
+++ cours4/Assets/Documents_etudiants_cours4/Script/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Door : MonoBehaviour {
+    [SerializeField] string sceneName;
+    [SerializeField] AudioClip stairSound;
+
+    AudioSource audioSource;
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        audioSource.PlayOneShot(stairSound);
+        Invoke("changeLevel", 1.0f);
+            }
+    private void changeLevel()
+    {
+        SceneManager.LoadScene(sceneName);
+    } 
+}
